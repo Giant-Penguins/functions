2df6099c7a620642c8a65688280d70d7c122e47c
Giant-Penguins <hcimpenguins@gmail.com>
2021-09-09 13:52:39 +0200

    working postman

--- APIs/todos.js
+++ APIs/todos.js
@@ -1,25 +1,57 @@
-const { db } = require('../util/admin');
+const { db } = require("../util/admin");
+
+//getAllTodos
 
 exports.getAllTodos = (request, response) => {
-	return db
-		.collection('todos')
-		.orderBy('createtat', 'desc')
-		.get()
-		.then((data) => {
-			let todos = [];
-			data.forEach((doc) => {
-                console.log("DEBUG", doc.id)
-				todos.push({
-                    todoId: doc.id,
-                    title: doc.data().title,
-					body: doc.data().body,
-					createtat: doc.data().createtat,
-				});
-			});
-			return response.json(todos);
-		})
-		.catch((err) => {
+  return db
+    .collection("todos")
+    .orderBy("createtat", "desc")
+    .get()
+    .then((data) => {
+      let todos = [];
+      data.forEach((doc) => {
+        console.log("DEBUG", doc.id);
+        todos.push({
+          todoId: doc.id,
+          title: doc.data().title,
+          body: doc.data().body,
+          createtat: doc.data().createtat,
+        });
+      });
+      return response.json(todos);
+    })
+    .catch((err) => {
+      console.error(err);
+      return response.status(500).json({ error: err.code });
+    });
+};
+
+//postOneTodo
+
+exports.postOneTodo = (request, response) => {
+	if (request.body.body.trim() === '') {
+		return response.status(400).json({ body: 'Must not be empty' });
+    }
+    
+    if(request.body.title.trim() === '') {
+        return response.status(400).json({ title: 'Must not be empty' });
+    }
+    
+    const newTodoItem = {
+        title: request.body.title,
+        body: request.body.body,
+        createdAt: new Date().toISOString()
+    }
+    db
+        .collection('todos')
+        .add(newTodoItem)
+        .then((doc)=>{
+            const responseTodoItem = newTodoItem;
+            responseTodoItem.id = doc.id;
+            return response.json(responseTodoItem);
+        })
+        .catch((err) => {
+			response.status(500).json({ error: 'Something went wrong' });
 			console.error(err);
-			return response.status(500).json({ error: err.code});
 		});
 };
